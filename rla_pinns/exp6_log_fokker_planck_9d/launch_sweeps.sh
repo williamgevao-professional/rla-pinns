8b68bd857803c88693cf5f9eb59aa2ca55b6ac15
Andrés Guzmán-Cordero <andresguzco@gmail.com>
2025-04-05 15:50:25 -0400

    Created new directories for experiments and it's respective sweep files

---
+++ rla_pinns/exp6_log_fokker_planck_9d/launch_sweeps.sh
@@ -0,0 +1,16 @@
+# Launch all sweeps using the sbatch command
+cd sweeps/
+
+# Benchamrks
+# sbatch SGD.sh
+# sbatch Adam.sh
+# sbatch ENGD.sh
+# sbatch HessianFree.sh
+
+# Woodbury ENGD
+sbatch ENGD_woodbury_exact.sh
+# sbatch ENGD_woodbury_nystrom.sh
+
+# Spring
+sbatch SPRING_exact.sh
+# sbatch SPRING_nystrom.sh
