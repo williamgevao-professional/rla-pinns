ab4ed978dbdfeeac2c7347cb4170d5e9d33b206f
Andrés Guzmán-Cordero <andresguzco@gmail.com>
2025-04-05 17:09:08 -0400

    New graphs for third experiment

--- rla_pinns/exp3_smallbatch/plot.py
+++ rla_pinns/exp3_smallbatch/plot.py
@@ -17,8 +17,8 @@ from rla_pinns.wandb_utils import (
     show_sweeps,
 )
 
-entity = "andresguzco"  # team name on wandb
-project = "rla-pinns"  # name from the 'Projects' tab on wandb
+entity = "rla-pinns"  # team name on wandb
+project = "exp3_smallbatch"  # name from the 'Projects' tab on wandb
 
 # information for title
 equation = "poisson"
@@ -38,14 +38,14 @@ if print_sweeps:
 
 
 sweep_ids = {  # ids from the wandb agent
-    "agtgmknd": "SGD",
-    "p6bgdypg": "Adam",
-    "fdohey43": "ENGD",
-    "d5ujt0u0": "Hessian-free",
-    "g9jjpfu4": "ENGD (Woodbury)",
-    "dvtd4rth": "ENGD (Nystrom)",
-    "hf4yth14": "SPRING",
-    "qf0s6jg3": "SPRING (Nystrom)",
+    # "agtgmknd": "SGD",
+    # "p6bgdypg": "Adam",
+    # "fdohey43": "ENGD",
+    # "d5ujt0u0": "Hessian-free",
+    "xocdtts9": "ENGD (Woodbury)",
+    # "dvtd4rth": "ENGD (Nystrom)",
+    "ihla13us": "SPRING",
+    # "qf0s6jg3": "SPRING (Nystrom)",
 }
 
 # color options: https://jiffyclub.github.io/palettable/colorbrewer/
