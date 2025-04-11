546935f3ede17c0448324f0822a38ae4c3b700fb
Andres <andresguzco@gmail.com>
2025-04-11 11:50:01 -0400

    Launching all optimizers in the aggresive small batch

--- rla_pinns/exp3_smallbatch/launch_sweeps.sh
+++ rla_pinns/exp3_smallbatch/launch_sweeps.sh
@@ -9,8 +9,8 @@ cd sweeps/
 
 # Woodbury ENGD
 sbatch ENGD_woodbury_exact.sh
-# sbatch ENGD_woodbury_nystrom.sh
+sbatch ENGD_woodbury_nystrom.sh
 
 # Spring
 sbatch SPRING_exact.sh
-# sbatch SPRING_nystrom.sh
+sbatch SPRING_nystrom.sh
--- rla_pinns/exp3_smallbatch/sweeps/SGD.sh
+++ rla_pinns/exp3_smallbatch/sweeps/SGD.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=rtx6000
-#SBATCH --qos=m3
+#SBATCH --qos=m
 
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=04:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --array=1-50%17
 
@@ -13,4 +13,4 @@ echo "[DEBUG] Host name: " `hostname`
 source  ~/miniforge3/etc/profile.d/conda.sh
 conda activate rla_pinns
 
-wandb agent --count 1 rla-pinns/exp3_smallbatch/a8t9o731
+wandb agent --count 1 rla-pinns/exp3_smallbatch/cy7vw0e4
