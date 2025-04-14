0d3ee75ee84b96cfb281eb1ef7441079bc3a7103
Andres <andresguzco@gmail.com>
2025-04-14 17:49:03 -0400

    Created exps 9 and 9 to show the performance of SPRING with fixed learning rate in the previous settings. Moving on to do the same for the log Fokker Planck setting. At last, the last experiments with very large batch sized is left to be done. Nystrom for all current settings is left to be done

---
+++ rla_pinns/exp8_poisson5d_fixedlr/create_sweeps.sh
@@ -0,0 +1,15 @@
+# Generate sweeps for all yaml config files
+# NOTE: This is usually only necessary once
+
+# BENCHMARKS
+# python ../yaml_to_sh.py sweeps/SGD.yaml sweeps/SGD.sh --qos=m2 --array=50
+# python ../yaml_to_sh.py sweeps/Adam.yaml sweeps/Adam.sh --qos=normal --array=50
+# python ../yaml_to_sh.py sweeps/ENGD.yaml sweeps/ENGD.sh --qos=m --array=50
+# python ../yaml_to_sh.py sweeps/HessianFree.yaml sweeps/HessianFree.sh --qos=m2 --array=50
+
+# WOODBURY ENGD
+python ../yaml_to_sh.py sweeps/ENGD_woodbury.yaml sweeps/ENGD_woodbury.sh --qos=m3 --array=50
+python ../yaml_to_sh.py sweeps/SPRING.yaml sweeps/SPRING.sh --qos=m4 --array=50
+# python ../yaml_to_sh.py sweeps/ENGD_nystrom.yaml sweeps/ENGD_nystrom.sh --qos=m2 --array=50
+# python ../yaml_to_sh.py sweeps/SPRING_nystrom.yaml sweeps/SPRING_nystrom.sh --qos=m --array=50
+# python ../yaml_to_sh.py sweeps/SPRING_fixed.yaml sweeps/SPRING_fixed.sh --qos=m2 --array=50
