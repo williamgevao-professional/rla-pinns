486d650cf684c9d2b758d9cd7662052ecf0568b5
Andres <andresguzco@gmail.com>
2025-04-14 10:05:01 -0400

    Added params for exp 4 with the 100d Poisson

--- rla_pinns/exp2_poisson10d/launch_sweeps.sh
+++ rla_pinns/exp2_poisson10d/launch_sweeps.sh
@@ -2,13 +2,13 @@
 cd sweeps/
 
 # Benchamrks
-sbatch SGD.sh
-sbatch Adam.sh
-sbatch ENGD.sh
-sbatch HessianFree.sh
+# sbatch SGD.sh
+# sbatch Adam.sh
+# sbatch ENGD.sh
+# sbatch HessianFree.sh
 
 # Woodbury ENGD & SPRING
-# sbatch ENGD_woodbury_exact.sh
+sbatch ENGD_woodbury_exact.sh
+sbatch SPRING_exact.sh
 # sbatch ENGD_woodbury_nystrom.sh
-# sbatch SPRING_exact.sh
 # sbatch SPRING_nystrom.sh
--- rla_pinns/exp4_poisson100d/launch_sweeps.sh
+++ rla_pinns/exp4_poisson100d/launch_sweeps.sh
@@ -8,9 +8,7 @@ cd sweeps/
 # sbatch HessianFree.sh
 
 # Woodbury ENGD
-sbatch ENGD_woodbury_exact.sh
+sbatch ENGD_woodbury.sh
+sbatch SPRING.sh
 # sbatch ENGD_woodbury_nystrom.sh
-
-# Spring
-sbatch SPRING_exact.sh
 # sbatch SPRING_nystrom.sh
