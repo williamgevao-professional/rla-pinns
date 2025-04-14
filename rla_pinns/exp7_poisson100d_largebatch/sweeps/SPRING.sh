d6c4debbc217fef6f1be8e4be6219ec2e6226138
Andres <andresguzco@gmail.com>
2025-04-14 16:27:02 -0400

    Added swepps for exp7

---
+++ rla_pinns/exp7_poisson100d_largebatch/sweeps/SPRING.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=rtx6000
+#SBATCH --qos=m4
+
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=02:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-50%17
+
+echo "[DEBUG] Host name: " `hostname`
+
+source  ~/miniforge3/etc/profile.d/conda.sh
+conda activate rla_pinns
+
+wandb agent --count 1 rla-pinns/exp7_poisson100d_largebatch/u93rvvyv
