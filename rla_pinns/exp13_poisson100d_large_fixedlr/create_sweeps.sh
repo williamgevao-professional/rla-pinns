c185f59a81b90df91b20c396cc367c0918012b71
Andrés Guzmán-Cordero <andresguzco@gmail.com>
2025-05-13 00:15:02 +0200

    Syncing exp 13 to cluster

---
+++ rla_pinns/exp13_poisson100d_large_fixedlr/create_sweeps.sh
@@ -0,0 +1,10 @@
+# Generate sweeps for all yaml config files
+# NOTE: This is usually only necessary once
+
+python ../yaml_to_sh.py sweeps/SPRING_100.yaml sweeps/SPRING_100.sh --qos=m2 --array=20
+python ../yaml_to_sh.py sweeps/SPRING_500.yaml sweeps/SPRING_500.sh --qos=m3 --array=20
+python ../yaml_to_sh.py sweeps/SPRING_1k.yaml sweeps/SPRING_1k.sh --qos=m4 --array=20
+
+python ../yaml_to_sh.py sweeps/SPRING_nystrom_100.yaml sweeps/SPRING_nystrom_100.sh --qos=m2 --array=20
+python ../yaml_to_sh.py sweeps/SPRING_nystrom_500.yaml sweeps/SPRING_nystrom_500.sh --qos=m3 --array=20
+python ../yaml_to_sh.py sweeps/SPRING_nystrom_1k.yaml sweeps/SPRING_nystrom_1k.sh --qos=m4 --array=20
---
+++ rla_pinns/exp13_poisson100d_large_fixedlr/launch_sweeps.sh
@@ -0,0 +1,11 @@
+# Launch all sweeps using the sbatch command
+cd sweeps/
+
+sbatch SPRING_1k.sh
+sbatch SPRING_500.sh
+sbatch SPRING_100.sh
+
+sbatch SPRING_nystrom_1k.sh
+sbatch SPRING_nystrom_500.sh
+sbatch SPRING_nystrom_100.sh
+
